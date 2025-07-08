3e8e6d9084d4184d846b44a45c6003ddb8809aa6
Harsh Patil <itsharshhh6@gmail.com>
2025-07-08 16:07:31 +0530

    Advancing with Tailwind CSS in React | Responsive Design and Custom Theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1 class="bg-green-600 text-3xl font-bold underline">
-             Hello world!
-      </h1>
+    <div className='bg-gray-500 p-4'>
+      <ul className='list-none md:flex md:gap-4'>
+        <li className='bg-amber-400 text-amber-700 p-2 rounded-lg m-2 md:px-4'>Item 1</li>
+        <li className='bg-amber-400 text-amber-700 p-2 rounded-lg m-2'>Item 2</li>
+        <li className='bg-amber-400 text-amber-700 p-2 rounded-lg m-2'>Item 3</li>
+      </ul>
     </div>
   )
 }
